611fd87574505bf2569ae5461afc8521d0ed4962
Nevin Baiju <nevinbaiju@gmail.com>
2023-04-23 16:41:01 -0400

    reflect segment naming and added first plots

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 from pages.predictions import midsize
+from pages.analyze_resale import analyze_resale_value
 from config.page_settings import SEGMENTS
 
 st.set_page_config(layout="wide")
@@ -8,19 +9,21 @@ st.set_page_config(layout="wide")
 tab1, tab2, tab3 = st.tabs(["Estimate resale value", "Trends in Resale value", "Analyze cars"])
 
 with tab1:
-    option = st.selectbox(
+    segment = st.selectbox(
         'Select the segment of cars you want to analyze',
         SEGMENTS, key='1')
 
-    midsize()
+    midsize(segment)
 
 with tab2:
-    option = st.selectbox(
+    segment = st.selectbox(
         'Select the segment of cars you want to analyze',
         SEGMENTS, key='2')
+    
+    analyze_resale_value(segment)
 
 with tab3:
-    option = st.selectbox(
+    segment = st.selectbox(
         'Select the segment of cars you want to analyze',
         SEGMENTS, key='3')
 
